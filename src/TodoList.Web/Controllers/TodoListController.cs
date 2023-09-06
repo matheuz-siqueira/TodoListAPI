4cbd969837298bddc07e0897a0ac99cdf7f1307f
matheuz-siqueira <matheusbatista.dev@gmail.com>
2023-09-05 21:08:53 -0300

    docs: adding swagger documentation

--- src/TodoList.Web/Controllers/TodoListController.cs
+++ src/TodoList.Web/Controllers/TodoListController.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace TodoList.Web.Controllers;
 
 [ApiController]
+[ApiVersion("1.0")]
 [Produces("application/json")]
 [ApiConventionType(typeof(DefaultApiConventions))]
 public class TodoListController : ControllerBase
