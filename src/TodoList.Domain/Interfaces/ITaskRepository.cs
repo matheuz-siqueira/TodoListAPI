1378f349335fdf0f294b520eff873f810bd3c4eb
matheuz-siqueira <matheusbatista.dev@gmail.com>
2023-09-14 23:40:55 -0300

    feat: removing task

--- src/TodoList.Domain/Interfaces/ITaskRepository.cs
+++ src/TodoList.Domain/Interfaces/ITaskRepository.cs
@@ -4,4 +4,6 @@ public interface ITaskRepository
     Task<TodoList.Domain.Models.Task> RegisterAsync(TodoList.Domain.Models.Task task);
     Task<IList<TodoList.Domain.Models.Task>> GetAllAsync(int userId);
     Task<TodoList.Domain.Models.Task> GetByIdAsync(int userId, int taskId);
+    Task<TodoList.Domain.Models.Task> GetByIdTracking(int userId, int taskId);
+    Task RemoveAsync(TodoList.Domain.Models.Task task);
 }
